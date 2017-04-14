86364c0ef11b71acc09301695f31bb70d07bc91b
pythonscraping <brice.jarmoszko@mail.mcgill.ca>
2017-04-13 21:54:08 -0400

    Update sql_tables.py

--- sql_tables.py
+++ sql_tables.py
@@ -2,8 +2,8 @@ import sqlite3
 conn = sqlite3.connect('movies.db')
 c = conn.cursor()
 
-#c.execute('''CREATE TABLE movies
-#             (imdbid text, movietitle TEXT, year INTEGER, genre text, imdbrating real, imdbvotes INTEGER,runtime REAL,release_date TEXT)''')
+c.execute('''CREATE TABLE movies
+             (imdbid text, movietitle TEXT, year INTEGER, genre text, imdbrating real, imdbvotes INTEGER,runtime REAL,release_date TEXT)''')
 c.execute('''CREATE TABLE tomato
              (imdbid text, 
              tomatometer TEXT, tomatoraveragerating TEXT, tomatoreviewcounted TEXT,
